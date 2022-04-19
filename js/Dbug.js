060fe3e855dc9c5025d417c0cf8b27f3eab58b7f
liuzhonghu <liuzhonghu521@gmail.com>
2022-04-19 21:32:26 +0800

    我的心分支

--- js/Dbug.js
+++ js/Dbug.js
@@ -1,12 +1,27 @@
-function Test() {
-    return ( 
-        <div>
-            这是我的测试分支，不需要进行多功能的打包,
-            我最新添加的分支是什么分支,这是二分之的语句,
-            xinjiade 
-            我是新的分支
-        </div>
-     );
-}
 
-export default Test;
+urlActionMulDownload= '/unionFile/downloadZip'
+const ids = [1,3,4,5,6,6,7,9]
+
+postUrl=(url, params)=> {
+    console.log(1);
+    // var temp_form = document.createElement("form");
+    // temp_form.action = url;
+    // temp_form.target = "_self";
+    // temp_form.method = "post";
+    // temp_form.style.display = "none";
+    console.log(params);
+    for (var x in params) {
+    //   var opt = document.createElement("input")
+    //   opt.name = x;
+    //   opt.value = params[x];
+    //   temp_form.appendChild(opt)
+    console.log(x);
+    }
+    // document.body.appendChild(temp_form);
+
+    // temp_form.submit()
+  }
+  mulDownload=()=> {
+    postUrl(urlActionMulDownload, {"fileIds": /* JSON.stringify(ids) */ids})
+  }
+  mulDownload()
